4bafd273d3d63e1ff153a5fc46ac0136881701b2
dmred <dmredchenko@gmail.com>
2016-04-19 20:07:11 +0300

    Update init.cpp

--- test/init.cpp
+++ test/init.cpp
@@ -31,6 +31,7 @@ SCENARIO("Matrix operator >>", "[Fill]")
 {
 	std::ifstream input("A.txt");
 	Matrix<int> A(2, 2);
+	REQUIRE( input >>  );
 	REQUIRE( input >> A );
 	REQUIRE( A[0][0] == 2 );
 	REQUIRE( A[0][1] == 3 );
